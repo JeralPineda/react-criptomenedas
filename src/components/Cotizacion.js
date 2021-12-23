710e314213c590619e802adf4741743abf97ce09
Jeral <jeral.cris@gmail.com>
2021-12-23 17:28:09 -0600

    Agregando styled components a la Cotizacion

--- src/components/Cotizacion.js
+++ src/components/Cotizacion.js
@@ -1,26 +1,50 @@
+import styled from '@emotion/styled';
+
+const ResultadoDiv = styled.div`
+   background-color: #fff;
+   padding: 4px 20px;
+   /* color: #000; */
+   border-radius: 10px;
+   margin-top: 20px;
+`;
+
+const Info = styled.p`
+   font-size: 18px;
+
+   span {
+      font-weight: bold;
+   }
+`;
+
+const Precio = styled.p`
+   font-size: 30px;
+
+   span {
+      font-weight: bold;
+   }
+`;
+
 const Cotizacion = ({ result }) => {
    if (Object.keys(result).length === 0) return null;
 
-   console.log(result);
-
    return (
-      <div>
-         <p>
+      <ResultadoDiv>
+         <Precio>
             El precio es: <span>{result.PRICE}</span>
-         </p>
-         <p>
+         </Precio>
+         <Info>
             Precio mas alto del día: <span>{result.HIGHDAY}</span>
-         </p>
-         <p>
+         </Info>
+         <Info>
             Precio mas bajo del día: <span>{result.LOWDAY}</span>
-         </p>
-         <p>
+         </Info>
+         <Info>
             Variacion ultimas 24 horas: <span>{result.CHANGEPCT24HOUR}</span>
-         </p>
-         <p>
+         </Info>
+         <Info>
             Ultima actualizacion: <span>{result.LASTUPDATE}</span>
-         </p>
-      </div>
+         </Info>
+      </ResultadoDiv>
    );
 };
 
